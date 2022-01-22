00ab46a0f60a80afb84cf3f129ccb8ae03edcab6
Daniel <itzdanielmichael@gmail.com>
2022-01-21 23:10:28 -0500

    fix moving object not being destroyed when off screen

--- Assets/Scripts/Objects/MovingObject.cs
+++ Assets/Scripts/Objects/MovingObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MovingObject : MonoBehaviour
 {
   public float moveSpeed;
+  private bool readyToRemove;
 
 
   // Update is called once per frame
@@ -13,11 +14,19 @@ public class MovingObject : MonoBehaviour
     transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
   }
 
+  void OnBecameVisible()
+  {
+    readyToRemove = true;
+  }
+
   /// <summary>
   /// OnBecameInvisible is called when the renderer is no longer visible by any camera.
   /// </summary>
   private void OnBecameInvisible()
   {
-    // Destroy(gameObject);
+    if (readyToRemove)
+    {
+      Destroy(gameObject);
+    }
   }
 }

64ac3b4814321a7fe224448a46e4e6932e576636
Daniel <itzdanielmichael@gmail.com>
2022-01-22 22:14:07 -0500

    add shot impact effect and DestoryOVerTime script

---
+++ Assets/Scripts/Misc/DestroyOverTime.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour
+{
+  public float lifeTime;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    // lifeTime -= Time.deltaTime;
+
+    // if (lifeTime <= 0)
+    // {
+    //   Destroy(gameObject);
+    // }
+
+
+    Destroy(gameObject, lifeTime);  // this does the same thing as the lines above
+  }
+}

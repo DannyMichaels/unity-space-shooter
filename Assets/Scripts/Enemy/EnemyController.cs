cc5b8352ebc1a38d0674b29eaa45b8567d0fa6b6
Daniel <itzdanielmichael@gmail.com>
2022-01-29 14:09:06 -0500

    add more movement to EnemyController

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -6,7 +6,11 @@ public class EnemyController : MonoBehaviour
 {
   public float moveSpeed;
 
-  public Vector2 startDirection;
+  public Vector2 startDirection; // the initial direction enemy will be going
+
+  public bool shouldChangeDirection; // will only change direction if this bool is set to true
+  public float changeDirectionXPoint; // the point on the x axis where the enemy will start changing direction
+  public Vector2 changedDirection; // direction to go once the direction has been changed
 
   // Start is called before the first frame update
   void Start()
@@ -24,6 +28,32 @@ public class EnemyController : MonoBehaviour
   private void HandleEnemyMove()
   {
     // transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
-    transform.position += new Vector3(startDirection.x * moveSpeed * Time.deltaTime, startDirection.y * moveSpeed * Time.deltaTime, 0f);
+
+    if (!shouldChangeDirection)
+    {
+
+
+      transform.position += calcNewPosition(startDirection.x, startDirection.y);
+    }
+    else
+    {
+      if (transform.position.x > changeDirectionXPoint)
+      {
+        transform.position += calcNewPosition(startDirection.x, startDirection.y);
+      }
+      else
+      {
+        transform.position += calcNewPosition(changedDirection.x, changedDirection.y);
+      }
+    }
+  }
+
+
+  private Vector3 calcNewPosition(float x, float y)
+  {
+    float newXPos = x * moveSpeed * Time.deltaTime;
+    float newYPos = y * moveSpeed * Time.deltaTime;
+
+    return new Vector3(newXPos, newYPos, 0f);
   }
 }

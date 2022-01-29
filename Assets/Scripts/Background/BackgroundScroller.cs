f58c8fa237e8c5e9a8308d53f2395f77f3ef6c75
Daniel <itzdanielmichael@gmail.com>
2022-01-29 13:58:57 -0500

    update Enemy Controller

--- Assets/Scripts/Background/BackgroundScroller.cs
+++ Assets/Scripts/Background/BackgroundScroller.cs
@@ -27,8 +27,8 @@ public class BackgroundScroller : MonoBehaviour
   private void ScrollBackground()
   {
     // BG1.position = new Vector3(BG1.position.x - (scrollSpeed * Time.deltaTime), BG1.position.y, BG1.position.z);
-    BG1.position -= new Vector3(scrollSpeed * Time.deltaTime, BG1.position.y, BG1.position.z); // this is the same code as above but written differently
-    BG2.position -= new Vector3(scrollSpeed * Time.deltaTime, BG2.position.y, BG2.position.z);
+    BG1.position -= new Vector3(scrollSpeed * Time.deltaTime, BG1.position.y, 0f); // this is the same code as above but written differently
+    BG2.position -= new Vector3(scrollSpeed * Time.deltaTime, BG2.position.y, 0f);
 
     // if bg1 is moved so far that it's off the screen, move it to the right ("infinite scroll" feel), same with bg2
     HandleMoveToEnd(BG1);
@@ -40,7 +40,7 @@ public class BackgroundScroller : MonoBehaviour
     // if bg is moved so far that it's off the screen, move it to the right
     if (bg.position.x < -bgWidth - 1)
     {
-      bg.position += new Vector3(bgWidth * 2f, bg.position.y, bg.position.z);
+      bg.position += new Vector3(bgWidth * 2f, bg.position.y, 0f);
     }
   }
 }
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -6,6 +6,8 @@ public class EnemyController : MonoBehaviour
 {
   public float moveSpeed;
 
+  public Vector2 startDirection;
+
   // Start is called before the first frame update
   void Start()
   {
@@ -21,10 +23,7 @@ public class EnemyController : MonoBehaviour
 
   private void HandleEnemyMove()
   {
-    transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
-
+    // transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+    transform.position += new Vector3(startDirection.x * moveSpeed * Time.deltaTime, startDirection.y * moveSpeed * Time.deltaTime, 0f);
   }
-
-
-
 }

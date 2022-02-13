1e7dc69feadb505700bbedf3459ce92cad1aabec
Daniel <itzdanielmichael@gmail.com>
2022-02-13 18:45:13 -0500

    add player respawn Invincibility

--- Assets/Scripts/Player/PlayerHealthController.cs
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -11,6 +11,10 @@ public class PlayerHealthController : MonoBehaviour
   public static PlayerHealthController instance; // instance = Singleton: create a version of this script that only one version of it can exist
   public GameObject deathEffect;
 
+  public float invincibilityDuration = 2f;
+  private float invincibleCounter;
+  public SpriteRenderer theSR;
+
   void Awake()
   {
     instance = this;
@@ -25,11 +29,14 @@ public class PlayerHealthController : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-
+    UseInvincibility();
   }
 
   public void DamagePlayer()
   {
+    bool canTakeDamage = invincibleCounter <= 0;
+    if (!canTakeDamage) return;
+
     currentHealth -= 1;
 
     if (currentHealth <= 0)
@@ -50,5 +57,30 @@ public class PlayerHealthController : MonoBehaviour
   {
     gameObject.SetActive(true);
     currentHealth = maxHealth;
+
+    MakePlayerInvincible();
+  }
+
+  private void MakePlayerInvincible()
+  {
+    invincibleCounter = invincibilityDuration;
+    ChangePlayerTransparency(.5f);
+  }
+
+  private void ChangePlayerTransparency(float alpha)
+  {
+    theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, alpha); // change transparency so player knows he's invincible or not
+  }
+
+  private void UseInvincibility()
+  {
+    if (invincibleCounter >= 0)
+    {
+      invincibleCounter -= Time.deltaTime;
+      if (invincibleCounter <= 0)
+      {
+        ChangePlayerTransparency(1f); // reset to 1.
+      }
+    }
   }
 }
